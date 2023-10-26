5212856a1abaf44bb138315fa2f7beba089b9d6e
chinnapareddy <chinna.aduri@gmail.com>
2023-10-25 20:41:32 -0700

    Terraform DNS Record

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "subnet_ids" {}
+variable "tags" {}
+variable "env" {}
+variable "vpc_id" {}
+variable "component" {}
+variable "port" {}
+variable "sg_ingress_cidr" {}
+variable "ssh_ingress_cidr" {}
+variable "zone_id" {}
