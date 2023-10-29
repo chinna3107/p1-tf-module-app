f1e392119a0adad9b499bea576196e1688e10d6c
chinnapareddy <chinna.aduri@gmail.com>
2023-10-29 02:04:59 -0700

    Terraform App

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 yum install  ansible python3.11-pip.noarch -y >>/opt/userdata.log
-pip3.11 install botocare boto3 &>>/opt/userdata.log
+#pip3.11 install botocare boto3 &>>/opt/userdata.log
+ pip install botocore==1.8./43 &>>/opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/chinna3107/p1-roboshop-ansible1.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
 
