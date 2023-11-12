a7e2afaaadcbaee43da3757c85faacce4d991e5b
chinnapareddy <chinna.aduri@gmail.com>
2023-11-11 22:09:51 -0800

    Terraform App

--- main.tf
+++ main.tf
@@ -166,7 +166,7 @@ resource "aws_lb_target_group" "public" {
   protocol = "HTTP"
   vpc_id   = var.default_vpc_id
 }
-/*
+
 resource "aws_lb_target_group_attachment" "public" {
    #count            = var.component == "frontend" ? length(var.subnet_ids) : 0
   count            = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
@@ -175,7 +175,7 @@ resource "aws_lb_target_group_attachment" "public" {
   port             = 80
   availability_zone = "all"
 }
-
+/*
 resource "aws_lb_listener_rule" "public" {
   count        = var.component == "frontend" ? 1 : 0
   listener_arn = var.public_listener
