1d318c86cfb10a829295cb728cc933bfb55f83dc
chinnapareddy <chinna.aduri@gmail.com>
2023-11-11 22:04:58 -0800

    Terraform App

--- main.tf
+++ main.tf
@@ -157,7 +157,7 @@ resource "aws_lb_listener_rule" "main" {
     }
   }
 }
-
+/*
 resource "aws_lb_target_group" "public" {
   count    = var.component == "frontend" ? 1 : 0
   name     = "${local.name_prefix}-public"
@@ -166,7 +166,7 @@ resource "aws_lb_target_group" "public" {
   protocol = "HTTP"
   vpc_id   = var.default_vpc_id
 }
-/*
+
 resource "aws_lb_target_group_attachment" "public" {
    #count            = var.component == "frontend" ? length(var.subnet_ids) : 0
   count            = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
