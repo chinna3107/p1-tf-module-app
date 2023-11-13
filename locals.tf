e57daaaf869c2982a868010f20c60ae2802bf6d2
chinnapareddy <chinna.aduri@gmail.com>
2023-11-13 03:35:19 -0800

    Terraform App

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  name_prefix = "${var.env}-${var.component}"
+  name_prefix = "${var.component}-${var.env}"
   tags = merge(var.tags, { tf-module-name = "app" }, { env = var.env })
   parameters   = concat(var.parameters, [var.component])
   policy_resource = [ for i in local.parameters: "arn:aws:ssm:us-east-1:683468297985:parameter/${i}.${var.env}.*"]
