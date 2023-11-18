96c4dc04cacacb19d1c897c0ee28f7a67cdfa925
chinnapareddy <chinna.aduri@gmail.com>
2023-11-18 06:12:14 -0800

    Terraform App

--- locals.tf
+++ locals.tf
@@ -1,6 +1,5 @@
 locals {
- # name_prefix = "${var.env}-${var.component}"
-  name_prefix = "${var.component}-${var.env}"
+  name_prefix = "${var.env}-${var.component}"
 
   tags = merge(var.tags, { tf-module-name = "app" }, { env = var.env })
   parameters   = concat(var.parameters, [var.component])
