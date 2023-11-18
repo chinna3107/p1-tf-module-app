747f2f3a68cf031ff63ee8e24c94e7ca63cf3b7e
chinnapareddy <chinna.aduri@gmail.com>
2023-11-18 03:56:02 -0800

    Terraform App

--- userdata.sh
+++ userdata.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
    set-hostname ${component}
+
    labauto ansible >>/opt/userdata.log
 
-  # yum install  ansible python3.12-pip.noarch -y >>/opt/userdata.log
+  # yum install  ansible python3.11-pip.noarch -y >>/opt/userdata.log
  #  yum install  ansible -y >>/opt/userdata.log
   # pip3.11 install botocare boto3 &>>/opt/userdata.log
   # pip3.11 install boto3 &>>/opt/userdata.log
