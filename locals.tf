0ebf35e1234fdafad0df50de13b05e36209959e5
chinnapareddy <chinna.aduri@gmail.com>
2023-11-29 02:22:53 -0800

    Roboshop prometheus

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   name_prefix = "${var.env}-${var.component}"
 
-  tags = merge(var.tags, { tf-module-name = "app" }, { env = var.env })
+  tags = merge(var.tags, { tf-module-name = "app" }, { env = var.env }, {Monitor = "yes"})
   parameters   = concat(var.parameters, [var.component])
   policy_resource = [ for i in local.parameters: "arn:aws:ssm:us-east-1:683468297985:parameter/${i}.${var.env}.*"]
 }
