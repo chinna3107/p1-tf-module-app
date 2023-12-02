278b21e8cbbfae89272bb21f3660dae12cf88828
chinnapareddy <chinna.aduri@gmail.com>
2023-12-02 08:54:00 -0800

    Roboshop prometheus

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_security_group_rule" "nginx_exporter" {
   from_port         = 9113
   to_port           = 9113
   protocol          = "tcp"
-  cidr_blocks       = [var.monitoring_ingress_cidr]
+  cidr_blocks       = var.monitoring_ingress_cidr
   security_group_id = aws_security_group.main.id
   description       = "Nginx Prometheus Exporter"
 }
